0c92d3fb265bf44f2426fd3b57bbca197bf44b52
Miguel Delgado <miguel.delg2004@gmail.com>
2026-04-27 23:36:54 -0300

    feat(Events/Evently.Modules.Events.Domain): add Event domain events -> canceled, published and rescheduled

--- src/Modules/Events/Evently.Modules.Events.Domain/Events/Event.cs
+++ src/Modules/Events/Evently.Modules.Events.Domain/Events/Event.cs
@@ -8,7 +8,7 @@ public sealed class Event : Entity
     private Event()
     {}
 
-    public Guid Id { get; private set; }
+    public Guid Id { get; private init; }
     
     public Guid CategoryId { get; private set; }
 
@@ -52,5 +52,51 @@ public sealed class Event : Entity
         @event.Raise(new EventCreatedDomainEvent(@event.Id));
 
         return @event;
+    }
+    
+    public Result Publish()
+    {
+        if (Status != EventStatus.Draft)
+        {
+            return Result.Failure(EventErrors.NotDraft);
+        }
+
+        Status = EventStatus.Published;
+
+        Raise(new EventPublishedDomainEvent(Id));
+
+        return Result.Success();
+    }
+
+    public void Reschedule(DateTime startsAtUtc, DateTime? endsAtUtc)
+    {
+        if (StartsAtUtc == startsAtUtc && EndsAtUtc == endsAtUtc)
+        {
+            return;
+        }
+
+        StartsAtUtc = startsAtUtc;
+        EndsAtUtc = endsAtUtc;
+
+        Raise(new EventRescheduledDomainEvent(Id, StartsAtUtc, EndsAtUtc));
+    }
+
+    public Result Cancel(DateTime utcNow)
+    {
+        if (Status == EventStatus.Canceled)
+        {
+            return Result.Failure(EventErrors.AlreadyCanceled);
+        }
+
+        if (StartsAtUtc < utcNow)
+        {
+            return Result.Failure(EventErrors.AlreadyStarted);
+        }
+
+        Status = EventStatus.Canceled;
+
+        Raise(new EventCanceledDomainEvent(Id));
+
+        return Result.Success();
     }
 }
