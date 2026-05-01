5de2e86676e72c30ac3707aa79d4e73651f3fc0b
Miguel Delgado <miguel.delg2004@gmail.com>
2026-05-01 20:55:29 -0300

    feat(Modules/Events/Presentation) add Category and TicketTypes tags

--- src/Modules/Events/Evently.Modules.Events.Presentation/Tags.cs
+++ src/Modules/Events/Evently.Modules.Events.Presentation/Tags.cs
@@ -3,4 +3,6 @@ namespace Evently.Modules.Events.Presentation;
 internal static class Tags
 {
     internal const string Events = "Events";
+    internal const string Category = "Category";
+    internal const string TicketTypes = "TicketTypes";
 }
