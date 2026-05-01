07921ae09d3e2c6137ff7a6d87f16d4355526765
Miguel Delgado <miguel.delg2004@gmail.com>
2026-05-01 20:53:26 -0300

    feat(Modules/Events/Presentation): add Categories and TicketTypes endpoints

---
+++ src/Modules/Events/Evently.Modules.Events.Presentation/Categories/CategoryEndpoints.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Events.Presentation.Categories;
+
+public abstract class CategoryEndpoints
+{
+    public static void MapEndpoints(IEndpointRouteBuilder app)
+    {
+        GetCategory.MapEndpoint(app);
+    }
+}
---
+++ src/Modules/Events/Evently.Modules.Events.Presentation/TicketTypes/TicketTypesEndpoints.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Events.Presentation.TicketTypes;
+
+public static class TicketTypesEndpoints
+{
+    public static void MapEndpoints(IEndpointRouteBuilder app)
+    {
+        GetTicketTypes.MapEndpoint(app);
+    }
+}
